d65451623c5e7027725d540eadbc4fc3a3c0e0a1
Wondr <wondr.design@gmail.com>
2025-03-08 23:37:51 +0100

    New

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 const App = () => {
   return (
     <>
-      <div className="text-blue-600">
-        <h1 className="text-h1">App</h1>
-      </div>
+      <section
+        className="bg-amber-800 w-screen h-screen"
+        style={{ overflow: "hidden" }}
+      >
+        <div className="bg-background text-foreground">
+          <h1 className="text-h1">Hello, Vite!</h1>
+          <div className=" bg-bg-card h-96 w-96 shadow-xl mt-[10rem]"></div>
+        </div>
+      </section>
+      <section
+        className="bg-amber-800 w-screen h-screen"
+        style={{ overflow: "hidden" }}
+      >
+        <div className="bg-background text-foreground">
+          <h1 className="text-h1">Hello, Vite!</h1>
+          <div className=" bg-bg-card h-96 w-96 shadow-xl mt-[10rem]"></div>
+        </div>
+      </section>
     </>
   );
 };
