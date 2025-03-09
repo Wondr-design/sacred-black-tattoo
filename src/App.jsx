b847ee6dac44e616804307d9a9a852bbdce0b625
Wondr <wondr.design@gmail.com>
2025-03-09 07:10:09 +0100

    Main second

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
           <h1 className="card selection:bg-amber-300 selection:text-amber-700">Chest nut color font </h1>
           <h2 className="card2">Chest nut color font </h2>
           <h3 className="card3">Chest nut color font </h3>
+          <h3 className="card3">Chest nut color font </h3>
+          <h3 className="card3">Chest nut color font </h3>
         </div>
 
       </section>
